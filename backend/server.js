daa40be18c51e4fdeeb3553ea2a321cb2fffe22f
HassanTAli <hassantarekha@gmail.com>
2022-09-11 13:59:35 +0200

    express app setup

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,23 @@
 // Lesson One : Express App Setup
 
+require("dotenv").config();
+
 const express = require("express");
 
 const app = express();
 
-app.listen(4000, () => {
+// middleware
+// => fancy name of code that execute between getting a request on server and us sending a response
+app.use((req, res, next) => {
+  console.log({ path: req.path, method: req.method });
+  next();
+});
+
+//routes
+app.get("/", (req, res) => {
+  res.json({ msg: "welcome to app" });
+});
+
+app.listen(process.env.PORT, () => {
   console.log("server run on port 4000");
 });
